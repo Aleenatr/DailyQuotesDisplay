73797868bb76a497491e4f90a41af0b0ec1956bc
aleenatr <aleenatr10@gmail.com>
2026-04-02 15:17:04 +0530

    Initial Commit

---
+++ DailyQuotesDisplay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyQuotesDisplay
+//
+//  Created by Aleena TR on 02/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
